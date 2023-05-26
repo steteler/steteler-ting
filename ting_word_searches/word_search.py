e5d21b1a327d5393810834ed342197a5912f5fa2
kikuti <joaovictorkikuti@gmail.com>
2023-05-26 17:11:25 -0300

    terminado requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    data = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        founds = []
+        for index, row in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in row.lower():
+                founds.append({'linha': index + 1})
+        if founds:
+            data.append({
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": founds
+            })
+
+    return data
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        founds = []
+        for index, row in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in row.lower():
+                founds.append({
+                    'linha': index + 1,
+                    'conteudo': row
+                })
+        if founds:
+            data.append({
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": founds
+            })
+
+    return data
