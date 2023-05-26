a165a5d046c3786833c881bf0a47db73637eda27
kikuti <joaovictorkikuti@gmail.com>
2023-05-26 14:46:28 -0300

    terminado requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+
+    try:
+        with open(path_file) as file:
+            rows = file.read().split('\n')
+            new_list = []
+            for row in rows:
+                new_list.append(row)
+            return new_list
+    except (FileNotFoundError):
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
